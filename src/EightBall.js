ffb5201375c0fb45780856a06730bc2c9c8e9545
NHower <howernickoli@gmail.com>
2022-11-02 17:06:56 -0400

    eight ball reset and counts

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,18 +4,36 @@ import './EightBall.css'
 const EightBall = ({ answers }) => {
     /* Randomizes an answer */
     const cycleAnswer = () => {
-        setAns(answers[Math.floor(Math.random() * answers.length)]);
+        const newAnswer = answers[Math.floor(Math.random() * answers.length)]
+        if(newAnswer.color === 'red') setR(r + 1);
+        if(newAnswer.color === 'goldenrod') setY(y + 1);
+        if(newAnswer.color === 'green') setG(g + 1);
+        setAns(newAnswer);
+        
+    }
+
+    const reset = () => { 
+        setAns({ msg: "Think of a Question", color: "black" }) 
     }
 
     const [ans, setAns] = useState({ msg: "Think of a Question", color: "black" });
+    const [r, setR] = useState(0);
+    const [g, setG] = useState(0);
+    const [y, setY] = useState(0);
 
     return (
-        <div
-            className="eightball"
-            onClick={ cycleAnswer }
-            style={{ backgroundColor: ans.color }}>
-            <h2>{ans.msg}</h2>
-        </div>
+        <>
+        <span>Red Count: {r} </span>
+        <span>Yellow Count: {y} </span>
+        <span>Green Count: {g} </span>
+            <div
+                className="eightball"
+                onClick={cycleAnswer}
+                style={{ backgroundColor: ans.color }}>
+                <h2>{ans.msg}</h2>
+            </div>
+            <button onClick={reset}>Reset</button>
+        </>
     );
 };
 
